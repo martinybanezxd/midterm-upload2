9d7dbe251ff293eb830925f024c5ab56d84765cc
martinybanezxd <57779413+martinybanezxd@users.noreply.github.com>
2020-01-24 11:31:19 +0800

    Add files via upload

---
+++ fileupload.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Uploading</title>
+</head>
+
+<body>
+<form method="post" action="uploadserver.php" enctype="multipart/form-data">
+<label for="covername">Upload Image</label>
+<?php echo "" ?>
+<input type="file" name="file">
+<input type="submit" value="Upload" name="submit">
+</form>
+</body>
+</html>
---
+++ indexupload.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+<a href="fileupload.php">
+	<button>Upload</button></a>
+	<?php echo "Upload Here!"; ?>
+</body>
+</html>
